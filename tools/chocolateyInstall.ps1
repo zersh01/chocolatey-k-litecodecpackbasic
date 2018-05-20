445d9cd15dacdffbc4e673c110dd152307b43cd6
Sergei Parhomenko <zersh@mail.ru>
2018-05-20 16:16:42 +0300

    first add files

---
+++ tools/chocolateyInstall.ps1
@@ -0,0 +1,35 @@
+﻿$packageName = 'k-litecodecpackbasic'
+$installerType = 'exe'
+$url = 'http://files2.codecguide.com/K-Lite_Codec_Pack_1415_Basic.exe'
+$silentArgs = '/VERYSILENT /NORESTART'
+                                         
+$checksum = '2c3807d5047e5e0851fa883ef4db95e7'
+
+$checksumType = 'md5'
+ 
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -Checksum "$checksum" -ChecksumType "$checksumType"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
