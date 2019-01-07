61427f90262d3c448086afb9739ab001d5f6ebb9
Sergei Parhomenko <zersh@mail.ru>
2019-01-07 18:26:19 +0300

    14.6.0

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,13 +1,19 @@
 ﻿$packageName = 'k-litecodecpackbasic'
 $installerType = 'exe'
-$url = 'http://files2.codecguide.com/K-Lite_Codec_Pack_1415_Basic.exe'
+$url = 'http://files2.codecguide.com/K-Lite_Codec_Pack_1460_Basic.exe'
 $silentArgs = '/VERYSILENT /NORESTART'
                                          
-$checksum = '2c3807d5047e5e0851fa883ef4db95e7'
+$checksum = '4cda080e8654668807b91a9aee23182e'
 
 $checksumType = 'md5'
  
 Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -Checksum "$checksum" -ChecksumType "$checksumType"
+
+
+
+
+
+
 
 
 
--- tools/chocolateyUninstall.ps1
+++ tools/chocolateyUninstall.ps1
@@ -32,3 +32,9 @@ if ($uninstallString -ne "") {
 
 
 
+
+
+
+
+
+
