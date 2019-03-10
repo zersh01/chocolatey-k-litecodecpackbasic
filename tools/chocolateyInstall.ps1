6d11732cb755bbee5eddb408ccf30ccc0bce9ba5
Sergei Parhomenko <zersh@mail.ru>
2019-03-10 22:59:24 +0300

    cdn.jsdelivr icon store, clean old packages

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,13 +1,15 @@
 ﻿$packageName = 'k-litecodecpackbasic'
 $installerType = 'exe'
-$url = 'http://files2.codecguide.com/K-Lite_Codec_Pack_1460_Basic.exe'
+$url = 'http://files2.codecguide.com/K-Lite_Codec_Pack_1475_Basic.exe'
 $silentArgs = '/VERYSILENT /NORESTART'
                                          
-$checksum = '4cda080e8654668807b91a9aee23182e'
+$checksum = '7051315e3fa33fbe089926a1057ace83'
 
 $checksumType = 'md5'
  
 Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -Checksum "$checksum" -ChecksumType "$checksumType"
+
+
 
 
 
