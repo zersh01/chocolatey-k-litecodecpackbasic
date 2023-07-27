7d152dd5996760ce1c992637ab670bc4bd7851ff
zersh01 <zersh01@gmail.com>
2023-07-27 23:47:25 +0300

    version 17.6.8

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,9 +1,9 @@
 ﻿$packageName = 'k-litecodecpackbasic'
 $installerType = 'exe'
-$url = 'https://files3.codecguide.com/K-Lite_Codec_Pack_1760_Basic.exe'
+$url = 'https://files3.codecguide.com/K-Lite_Codec_Pack_1768_Basic.exe'
 $silentArgs = '/VERYSILENT /NORESTART'
                                          
-$checksum = '5690db0fcf6cf5047c9c342643b67a8d'
+$checksum = '90dc4a73f44b1232a66e816ff9a82818'
 
 $checksumType = 'md5'
  
